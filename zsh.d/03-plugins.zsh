481575577c896cfabcd6440b01e43cea85950f2e
svpace <svpace@gmail.com>
2019-06-02 17:31:48 -0300

    nano

--- zsh.d/03-plugins.zsh
+++ zsh.d/03-plugins.zsh
@@ -1,2 +1,3 @@
 source ${HOME}/.dotfiles/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ${HOME}/.dotfiles/agnoster-zsh-theme/agnoster.zsh-theme
+source ${HOME}/.dotfiles/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
