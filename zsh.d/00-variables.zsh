26997e2cd8bde001aef0305d9397371374c7604b
svpace <svpace@gmail.com>
2019-06-02 02:48:43 -0300

    setup

--- zsh.d/00-variables.zsh
+++ zsh.d/00-variables.zsh
@@ -1,5 +1,5 @@
 HISTFILE=~/.histfile
-#HISTSIZE=1000
-#SAVEHIST=1000
+HISTSIZE=10000
+SAVEHIST=10000
 DEFAULT_USER='svpace'
 #export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
---
+++ zsh.d/03-plugins.zsh
@@ -0,0 +1,2 @@
+source ${HOME}/.dotfiles/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ${HOME}/.dotfiles/agnoster-zsh-theme/agnoster.zsh-theme
